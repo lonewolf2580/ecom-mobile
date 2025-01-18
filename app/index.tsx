b4cbb5732eb75e14d697ad806d106a9556ddc058
Linus Ngwee <linusngwee@yahoo.com>
2025-01-18 09:32:47 +0100

    Fix rerender

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,25 @@
-import { FlatList, Text, View } from "react-native";
+import { FlatList, useWindowDimensions, View } from "react-native";
 import products from '@/assets/products.json'
 import ProductList from "@/components/productListItem";
+import { useBreakpointValue } from "@/components/ui/utils/use-break-point-value";
 
 export default function HomeScreen() {
+  // const { width } = useWindowDimensions()
+  // const numColums = width > 700 ? 4 : 2
+
+  const numColumns = useBreakpointValue({
+    default: 2,
+    sm: 3,
+    xl: 4
+  })
+
   return (
     <View>
       <FlatList 
+        key={numColumns}
         data={products} 
-        numColumns={2}
-        contentContainerClassName="gap-2"
+        numColumns={numColumns}
+        contentContainerClassName="gap-2 max-w-[960px] mx-auto w-full"
         columnWrapperClassName="gap-2"
         renderItem={({item})=> {
         return <ProductList product={item} />
